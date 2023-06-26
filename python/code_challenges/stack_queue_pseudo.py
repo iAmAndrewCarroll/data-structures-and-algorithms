d475e58aa89a63e4971b852ca25d832664e9b83d
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-06-26 14:41:35 -0700

    passing

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,24 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, value):
+        self.stack1.append(value)
+
+    def dequeue(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+if __name__ == "__main__":
+    queue = PseudoQueue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
