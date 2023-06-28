926061ea3f9047da6f90d246033a57a7655f71b3
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-06-28 13:38:44 -0700

    all tests pass

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,21 @@
 from data_structures.queue import Queue
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    stack = []
+    open_brackets = ['(', '[', '{']
+    close_brackets = [')', ']', '}']
+    pair_brackets = {'(': ')', '[': ']', '{': '}'}
+
+    for brack in string:
+        if brack in open_brackets:
+            stack.append(brack)
+        elif brack in close_brackets:
+            if len(stack) == 0:
+                return False
+            if pair_brackets[stack[-1]] == brack:
+                stack.pop()
+            else:
+                return False
+
+    return len(stack) == 0
