f35a1079d528ef567f272f08de9d7fdfce1cb1ce
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-07-05 13:04:12 -0700

    passing

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,22 @@
 from data_structures.binary_tree import BinaryTree
+from collections import deque
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    if not tree.root:
+        return[]
+
+    result = []
+    queue = deque()
+    queue.append(tree.root)
+
+    while queue:
+        node = queue.popleft()
+        result.append(node.value)
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+    return result
