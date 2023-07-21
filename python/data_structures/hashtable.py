025e65123372dfce95d9c845588c1c776c47e822
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-07-21 13:13:14 -0700

    passing some tests

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,10 +1,16 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
 class Hashtable:
     """
     Paramaters: Key, Value
     Returns: Nothing
     """
 
-    def __init__(self, size=20):
+    def __init__(self, size=1024):
         self.size = size
         self.buckets = size * [None]
 
@@ -14,7 +20,7 @@ class Hashtable:
         Returns: nothing
         """
         # hash the key
-        index = self._hash(key)
+        index = self.hash(key)
         # determine index and go there
         if self.buckets[index] is None:
         # if there is nothing there, add a new node
@@ -33,6 +39,49 @@ class Hashtable:
         # if the key is different, add a new node
             current.next = Node(key, value)
 
+    # define the get method of the hashtable class. Takes a key as an argument and returns the associated value if the key is found in the hashtable. If the key is not found, return None.
+    def get(self, key):
+        """
+        Arguments: key
+        Returns: value
+        """
+        # hash the key
+        index = self.hash(key)
+        # check if there is no data (node) at the index in the hashtable (self.buckets). If index is empty, the key is not present in the hashtable, and the method should return None.
+        if self.buckets[index] is None:
+            return None
+        # if there is at least one node present at the index, there is the possibility of a collision. Traverse the linked list at that index to find the key/value pair.
+        else:
+            # create current variable and initialize it to the first node in the linked list at the index. This allows traversal of the linked list from the head node to find the key/value match.
+            current = self.buckets[index]
+            # while loop that iterates as long as there is a valid (current) node in the linked list.  Used to traverse through the linked list and check if the key exists.
+            while current:
+                # check if the current node's key matches the key that was passed into the get method. If so, return the value.
+                if current.key == key:
+                    return current.value
+                current = current.next
+
+    def has(self, key):
+        """
+        Arguments: key
+        Return: boolean
+        """
+        # hash the key
+        index = self.hash(key)
+        # check if there is no data (node) at the index in the hashtable (self.buckets). If index is empty, the key is not present in the hashtable, and the method should return None.
+        if self.buckets[index] is None:
+            return False
+        # if there is at least one node present at the index, there is the possibility of a collision. Traverse the linked list at that index to find the key/value pair.
+        else:
+            # create current variable and initialize it to the first node in the linked list at the index. This allows traversal of the linked list from the head node to find the key/value match.
+            current = self.buckets[index]
+            # while loop that iterates as long as there is a valid (current) node in the linked list.  Used to traverse through the linked list and check if the key exists.
+            while current:
+                # check if the current node's key matches the key that was passed into the get method. If so, return the value.
+                if current.key == key:
+                    return True
+                current = current.next
+            return False
 
 
 
