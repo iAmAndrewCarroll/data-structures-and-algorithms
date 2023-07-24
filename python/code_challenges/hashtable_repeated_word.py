2c13e2475381d104ae8ddf005ece79475815d0b8
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-07-24 15:17:00 -0700

    some pass

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,16 @@
+import re
 from data_structures.hashtable import Hashtable
 
+def parse_words(input_string):
+    return re.findall(r'\b[\w-]+\b', input_string.lower())
 
-def first_repeated_word():
-    pass
+def first_repeated_word(input_string):
+    words = parse_words(input_string)
+    word_count = Hashtable()
+
+    for word in words:
+        if word_count.has(word):
+            return word
+        word_count.set(word, 1)
+
+    return None
